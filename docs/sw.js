4c477f34be7cad9fd921616878478af1500c979e
adamkiryk <adamkiryk@gmail.com>
2018-02-16 18:29:55 -0500

    First commit

---
+++ docs/sw.js
@@ -0,0 +1,38 @@
+const staticCacheName = 'ak-cache-v11';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache){
+      return cache.addAll([
+        '/',
+        'test.html',
+        'about.html',
+        'css/main.css',
+        'js/main.js',
+      ])
+    })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      // delete all the caches except current one
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => {
+            return cacheName !== staticCacheName && cacheName.startsWith('ak-cache')
+          })
+          .map(cacheName => caches.delete(cacheName))
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request);
+    })
+  )
+})
---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+function IndexController() {
+  this.registerServiceWorker();
+}
+
+IndexController.prototype.registerServiceWorker = function() {
+  if(!navigator.serviceWorker) return;
+
+  navigator.serviceWorker.register('../sw.js').then(function(reg) {
+    console.log('success regisering sw');
+  }).catch(function() {
+    console.log('failed to register sw!');
+  })
+}
+
+
+// new IndexController();

46eccc0daed78fda00caf5882f634ce2d605af44
adamkiryk <adamkiryk@gmail.com>
2018-02-17 07:48:32 -0500

    remove typo

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ function IndexController() {
 IndexController.prototype.registerServiceWorker = function() {
   if(!navigator.serviceWorker) return;
 
-  navigator.serviceWorker.register('/{repository}/sw.js', {scope: '/{repository}/'})
   navigator.serviceWorker.register('https://akiryk.github.io/thirty-sites-3/sw.js', {scope: 'https://akiryk.github.io/thirty-sites-3/'}).then(function(reg) {
     console.log('success regisering sw');
   }).catch(function() {

4b6a2483701e70f8efe023b8f68503fc8098da69
adamkiryk <adamkiryk@gmail.com>
2018-02-17 07:46:22 -0500

    Add scope to sw

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,8 @@ function IndexController() {
 IndexController.prototype.registerServiceWorker = function() {
   if(!navigator.serviceWorker) return;
 
-  navigator.serviceWorker.register('../sw.js').then(function(reg) {
+  navigator.serviceWorker.register('/{repository}/sw.js', {scope: '/{repository}/'})
+  navigator.serviceWorker.register('https://akiryk.github.io/thirty-sites-3/sw.js', {scope: 'https://akiryk.github.io/thirty-sites-3/'}).then(function(reg) {
     console.log('success regisering sw');
   }).catch(function() {
     console.log('failed to register sw!');
